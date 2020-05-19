0ee9be34f179af311ff1e759fdfd5b0a1aabc76a
Leonardo Almeida <leo@leleo.com.br>
2020-05-19 20:53:06 +0200

    arduino workshop stuff

--- Workshop_files_begin/04Animation/scripts/main.js
+++ Workshop_files_begin/04Animation/scripts/main.js
@@ -1,12 +1,12 @@
+// changing individual properties with code and using setInterval
+var rotationSpeed = -0.09;
+var myOtherBox = document.getElementById('myOtherBox');
 
+function spin() {
+    myOtherBox.object3D.rotation.x += rotationSpeed;
+    myOtherBox.object3D.rotation.y += rotationSpeed;
+    myOtherBox.object3D.rotation.z += rotationSpeed;
+    console.log(myOtherBox.object3D.rotation.x);
+}
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
-
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
-
-// setInterval(spin, 16); //equivalent to 60 fps
+setInterval(spin, 16); //equivalent to 60 fps
--- Workshop_files_begin/06InteractionHeadset/scripts/main.js
+++ Workshop_files_begin/06InteractionHeadset/scripts/main.js
@@ -1,24 +1,23 @@
-
 var rotationSpeed = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
 
-function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	//console.log(myOtherBox.object3D.rotation);
+function spin() {
+    myOtherBox.object3D.rotation.x += rotationSpeed;
+    myOtherBox.object3D.rotation.y += rotationSpeed;
+    myOtherBox.object3D.rotation.z += rotationSpeed / 2;
+    //console.log(myOtherBox.object3D.rotation);
 }
 setInterval(spin, 10);
 
 
-myOtherBox.addEventListener('mouseenter', function(){
-	rotationSpeed = 0.01;
-	console.log('entered');
+myOtherBox.addEventListener('mouseenter', function() {
+    rotationSpeed = 0.01;
+    console.log('entered');
 });
 
-myOtherBox.addEventListener('mouseleave', function(){
-	rotationSpeed = 0.001;
-	console.log('left');
+myOtherBox.addEventListener('mouseleave', function() {
+    rotationSpeed = 0.001;
+    console.log('left');
 });
 
 
@@ -28,16 +27,16 @@ myOtherBox.addEventListener('mouseleave', function(){
  * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time. 
  */
 
-// var growspeed = 1.2;
+var growspeed = 1.2;
 
-// function grow(){
-// 	myOtherBox.object3D.scale.x *= growspeed;
-// 	myOtherBox.object3D.scale.y *= growspeed;
-// 	myOtherBox.object3D.scale.z *= growspeed;
-// 	//console.log(myOtherBox.object3D.scale);
-// }
+function grow() {
+    myOtherBox.object3D.scale.x += growspeed;
+    myOtherBox.object3D.scale.y += growspeed;
+    myOtherBox.object3D.scale.z += growspeed;
+    //console.log(myOtherBox.object3D.scale);
+}
 
-// myOtherBox.addEventListener('click', function(){ // uses a fuse
-// 	grow();
-// 	console.log('grew');
-// });
+myOtherBox.addEventListener('click', function() { // uses a fuse
+    grow();
+    console.log('grew');
+});

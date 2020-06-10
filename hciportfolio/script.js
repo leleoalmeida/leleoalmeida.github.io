43d696ba7baed1882b71c5485d726922f705b5cd
Leonardo Almeida <leo@leleo.com.br>
2020-06-10 11:42:20 +0200

    hci portfolio moved to notion

--- hciportfolio/script.js
+++
@@ -1,6 +0,0 @@
-function openImage(imageName) {
-  var modal = document.getElementById('myModal');
-  var modalImg = document.getElementById('img01');
-  modal.style.display = "block";
-  modalImg.src = imageName;
-}

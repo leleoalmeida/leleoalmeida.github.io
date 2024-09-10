22fe5a5dc37dc9f717c12ad4b4ede8852e1ab215
Leonardo Almeida <leo@leleo.com.br>
2024-09-10 16:32:59 +0200

    work images rework

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-const changeThumb = (prop) => {
-  const source = "../img/" + prop;
-  document.getElementById("work-thumb").src = source;
-  document.getElementById("work-thumb").style.display = "block";
-};
+const elements = document.getElementsByClassName("work")
+let mouseX = 0
+let mouseY = 0
 
-const resetThumb = () => {
-  document.getElementById("work-thumb").style.display = "none";
-};
+for (let i = 0; i < elements.length; i++) {
+  const element = elements[i];
+
+  element.addEventListener("mousemove", (e) => {
+    // console.log(e);
+    mouseX = e.clientX - 400
+    mouseY = e.clientY + 100
+    console.log(document.getElementById("work-thumb").style.top);
+
+
+    document.getElementById("work-thumb").style.top = mouseY + 'px'
+    document.getElementById("work-thumb").style.left = mouseX + 'px'
+  })
+
+  element.addEventListener("mouseleave", () => {
+    document.getElementById("work-thumb").style.display = "none";
+  })
+
+  element.addEventListener("mouseenter", (e) => {
+    document.getElementById("work-thumb").style.display = "block";
+
+    const source = "../img/thumbnails/" + i + ".jpg";
+    document.getElementById("work-thumb").src = source;
+  })
+}
